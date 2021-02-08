75f2764d48ead6c2ff1b1a1916efabe2d40da060
Guillaume P <gg.postec@laposte.net>
2021-02-08 14:14:28 +0100

    Récolte de l'url image + création d'un dictionnaires des données souhaitées à partir du premier dictionnaire ainsi que des autres données

--- scraping.py
+++ scraping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 book_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html" #product_page_url
+website_url = 'http://books.toscrape.com/'
 
 
 response = requests.get(book_url) #Récupération du code HTML d'un livre grâce à une requête sur son URL
@@ -13,15 +14,31 @@ if response.ok: #Vérification que la requête a réussi
     book_title = book_soup.find(class_="col-sm-6 product_main").find('h1').text #title
     p_list = book_soup.find_all('p')
     book_description = p_list[3].text #product_description
+    book_image = website_url + book_soup.find(class_="item active").find('img')['src'].strip("../") #image_url
 
     #Récupération des données du tableau du livre
     book_table_dictionary = {} #Création d'un premier dictionnaire pour récolter les données indiqué sur le site
+    book_dictionary = {} #Création d'un deuxième dictionnaire pour récolter les données souhaitées pour le porjet
 
     book_table_list = book_soup.find(class_="table table-striped").find_all('tr')
     for tr in book_table_list:
         book_table_dictionary[tr.th.text] = tr.td.text #Remplissage du premier dictionnaire à partir des informations du livre récoltées
 
+
+    
+    book_dictionary["product_page_url"] = book_url
+    book_dictionary["universal_product_code"] = book_table_dictionary["UPC"]
+    book_dictionary["title"] = book_title
+    book_dictionary["price_including_tax"] = book_table_dictionary["Price (incl. tax)"]
+    book_dictionary["price_excluding_tax"] = book_table_dictionary["Price (excl. tax)"]
+    book_dictionary["number_available"] = book_table_dictionary["Availability"]
+    book_dictionary["product_description"] = book_description
+    book_dictionary["category"] = '?'
+    book_dictionary["review_rating"] = '?'
+    book_dictionary["image_url"] = book_image
+
     #Affichage des données récoltées dans la console
-    print(book_table_dictionary)
+    print(book_dictionary)
+
 
     
