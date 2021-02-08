69e6b8f89039b80e1dd5b0cc4fab9c2b3e16c713
Guillaume P <gg.postec@laposte.net>
2021-02-08 14:07:39 +0100

    Rajout des données du tableau du livre

--- scraping.py
+++ scraping.py
@@ -13,10 +13,15 @@ if response.ok: #Vérification que la requête a réussi
     book_title = book_soup.find(class_="col-sm-6 product_main").find('h1').text #title
     p_list = book_soup.find_all('p')
     book_description = p_list[3].text #product_description
-    
+
+    #Récupération des données du tableau du livre
+    book_table_dictionary = {} #Création d'un premier dictionnaire pour récolter les données indiqué sur le site
+
+    book_table_list = book_soup.find(class_="table table-striped").find_all('tr')
+    for tr in book_table_list:
+        book_table_dictionary[tr.th.text] = tr.td.text #Remplissage du premier dictionnaire à partir des informations du livre récoltées
 
     #Affichage des données récoltées dans la console
-    print(book_url)
-    print(book_title)
-    print(book_description)
+    print(book_table_dictionary)
+
     
